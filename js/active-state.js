0f6b6c8c4ac44915974aa6f15a460cc842666f08
GwynethS <102232553+GwynethS@users.noreply.github.com>
2023-05-16 12:26:18 -0500

    G - BreadCrumb y active state

---
+++ js/active-state.js
@@ -0,0 +1,23 @@
+//NAVBAR
+
+const navLinksElements = document.querySelectorAll('.menu-item');
+const windowPathName = window.location.pathname;
+
+navLinksElements.forEach(e => {
+    const navLinkPathName = new URL(e.href).pathname;
+    if(windowPathName === navLinkPathName){
+        e.classList.add('active');
+    }
+});
+
+
+//BREADCRUMBS
+
+const breadCrumbLinksElements = document.querySelectorAll('.breadcrumbs-link');
+
+breadCrumbLinksElements.forEach(e => {
+    const breadLinkPathName = new URL(e.href).pathname;
+    if(windowPathName === breadLinkPathName){
+        e.classList.add('active');
+    }
+});
