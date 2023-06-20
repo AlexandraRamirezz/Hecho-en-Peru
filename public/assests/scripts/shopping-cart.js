7ec2f5f1adb209aafb1862b2051315638d5df04f
GwynethS <102232553+GwynethS@users.noreply.github.com>
2023-06-20 02:54:38 -0500

    remover y change quantity

--- public/assests/scripts/shopping-cart.js
+++ public/assests/scripts/shopping-cart.js
@@ -12,51 +12,75 @@ closeCart.onclick = () =>{
     cart.classList.remove("active");
 };
 
-//Remove Items From Cart
-var removeCartButtons = document.querySelectorAll(".remove-cart");
+if(document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+}else{
+    ready();
+}
 
-for (var i = 0; i < removeCartButtons.length; i++)
+function ready()
 {
-    var button = removeCartButtons[i];
-    button.addEventListener("click", function(e){
-        var buttonClicked = e.target;
-        buttonClicked.parentElement.remove();
-        updateTotal();
-    });
+    var removeCartButtons = document.getElementsByClassName("remove-cart")
+    for (var i = 0; i < removeCartButtons.length; i++)
+    {
+        var button = removeCartButtons[i];
+        button.addEventListener("click", removeCartItem);
+    }
+
+    //Quantity Changes
+    var quantityInputs = document.getElementsByClassName("unidades-selected");
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i];
+
+        var minusButton = input.previousElementSibling;
+        var plusButton = input.nextElementSibling;
+
+        minusButton.addEventListener("click", quitUnitProduct);
+        plusButton.addEventListener("click", addUnitProduct);
+    }
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updateTotal();
+}
+
+function quitUnitProduct() {
+    var input = this.nextElementSibling;
+    var currentValue = parseInt(input.value);
+    if (currentValue > 1) {
+        input.value = currentValue - 1;
+    }
+    updateTotal();
+}
+
+function addUnitProduct() {
+    var input = this.previousElementSibling;
+    var currentValue = parseInt(input.value);
+    input.value = currentValue + 1;
+    updateTotal();
 }
 
 //Update total
 
-const cartContent = document.querySelector(".cart-content");
-// Obtener todos los elementos con la clase "cart-box"
-const cartBoxes = cartContent.getElementsByClassName("cart-box");
-const emptyCart = document.getElementsByClassName("empty-cart")[0];
-const btnBuyCart = cartContent.getElementsByClassName("btn-buy-cart")[0];
-
 function updateTotal(){
-    //var cartBoxes = document.getElementsByClassName("cart-box");
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
     var total = 0;
-    if (cartBoxes.length == 0)
+    for (var i = 0; i < cartBoxes.length; i++)
     {
-        document.getElementsByClassName("total-price")[0].innerText = "s/. 0.00";
-        emptyCart.style.display = "block";
-        btnBuyCart.style.display = "none";
-    }else{
-        emptyCart.style.display = "none";
-        btnBuyCart.style.display = "block";
-        for (var i = 0; i < cartBoxes.length; i++){
-            var cartBox = cartBoxes[i];
-            var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-            var quantityElement = cartBox.getElementsByClassName("unidades-selected")[0];
-            var price = parseFloat(priceElement.innerText.replace("s/.",""));
-            var quantity = quantityElement.value;
-            console.log(quantityElement);
-            total = total + (price * quantity);
-    
-            document.getElementsByClassName("total-price")[0].innerText = "s/. " + total.toFixed(2);
-        }
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("unidades-selected")[0];
+        var price = parseFloat(priceElement.innerText.replace("s/.",""));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+
+        document.getElementsByClassName("total-price")[0].innerText = "s/." + total.toFixed(2);
     }
 }
+
 
 window.addEventListener("DOMContentLoaded", function() {
     // Llamar a la función para actualizar el número de comentarios
@@ -66,7 +90,7 @@ window.addEventListener("DOMContentLoaded", function() {
 //Número de unidades
 
 // Iterar sobre cada elemento del carrito
-for (var i = 0; i < cartBoxes.length; i++){
+/*for (var i = 0; i < cartBoxes.length; i++){
     var cartItem = cartBoxes[i];
     const plus = cartItem.querySelector(".plus");
     const minus = cartItem.querySelector(".minus");
@@ -87,4 +111,4 @@ for (var i = 0; i < cartBoxes.length; i++){
         }
         updateTotal();
     });
-}
+}*/
